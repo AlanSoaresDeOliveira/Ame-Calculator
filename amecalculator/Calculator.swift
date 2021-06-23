25c1dc0628adc575be824aa9fdc0a12e823a0794
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2021-06-23 15:44:04 -0300

    Create Calculator class

---
+++ amecalculator/Calculator.swift
@@ -0,0 +1,32 @@
+//
+//  Calculator.swift
+//  amecalculator
+//
+//  Created by Alan Soares de Oliveira on 23/06/21.
+//
+
+import Foundation
+
+enum CalculatorError: Error {
+    case defaultError
+    case divisionByZero
+}
+
+class Calculator {
+    
+    public func add(x: Float, y: Float) -> Float {
+        return 0
+    }
+    
+    public func substract(x: Float, y: Float) -> Float {
+        return 0
+    }
+    
+    public func multiply(x: Float, y: Float) -> Float {
+        return 0
+    }
+    
+    public func divide(x: Float, y: Float) throws -> Float {
+        return 0
+    }
+}
